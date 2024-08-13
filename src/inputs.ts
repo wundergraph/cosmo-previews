e0268e819920e4de4b4afb561b79cf7a202eb209
JivusAyrus <suvijsurya76@gmail.com>
2024-08-13 17:33:03 +0530

    feat: initial commit

---
+++ src/inputs.ts
@@ -0,0 +1,96 @@
+import * as core from '@actions/core';
+import * as yaml from 'js-yaml';
+import { existsSync, readFileSync } from 'node:fs';
+import { dirname, resolve } from 'pathe';
+
+export type Config = {
+  namespace: string;
+  feature_flags: FeatureFlag[];
+  subgraphs: {
+    name: string;
+    schema_path: string;
+    routing_url: string;
+  }[];
+};
+
+export type FeatureFlag = {
+  name: string;
+  labels: string[];
+};
+
+export type Subgraph = {
+  name: string;
+  schemaPath: string;
+  routingUrl: string;
+};
+
+export type ActionType = 'create' | 'update' | 'destroy';
+
+export type Inputs = {
+  cosmoApiKey: string;
+  actionType: ActionType;
+  namespace: string;
+  featureFlags: FeatureFlag[];
+  subgraphs: Subgraph[];
+};
+
+export const getInputs = (): Inputs | undefined => {
+  const configPath = core.getInput('config_path') || '.github/cosmo.yaml';
+  const cosmoApiKey = core.getInput('cosmo_api_key', { required: true });
+  const create = core.getInput('create') === 'true';
+  const update = core.getInput('update') === 'true';
+  const destroy = core.getInput('destroy') === 'true';
+
+  if (!create && !update && !destroy) {
+    core.setFailed('Please provide at least one action type to perform. Either create, update, or destroy.');
+    return;
+  }
+
+  // Ensure only one of create, update, or destroy is true
+  const trueCount = [create, update, destroy].filter((value) => value).length;
+  if (trueCount !== 1) {
+    core.setFailed('Exactly one of "create", "update", or "destroy" must be true.');
+    return;
+  }
+
+  const actionType = create ? 'create' : update ? 'update' : 'destroy';
+
+  const inputFile = resolve(process.cwd(), configPath);
+  const inputFileLocation = dirname(inputFile);
+
+  if (!existsSync(inputFile)) {
+    core.setFailed(`The input file '${inputFile}' does not exist. Please check the path.`);
+    return;
+  }
+
+  const fileContent = readFileSync(inputFile).toString();
+  const config = yaml.load(fileContent) as Config;
+
+  const namespace = config.namespace;
+
+  if (!config.feature_flags || config.feature_flags.length === 0) {
+    core.setFailed(`Please provide at least one feature flag in the config file '${inputFile}'.`);
+    return;
+  }
+  const featureFlags = config.feature_flags;
+
+  if (!config.subgraphs || config.subgraphs.length === 0) {
+    core.setFailed(`Please provide at least one subgraph in the config file '${inputFile}'.`);
+    return;
+  }
+  const subgraphs = config.subgraphs.map((subgraph) => {
+    return {
+      name: subgraph.name,
+      schemaPath: resolve(inputFileLocation, subgraph.schema_path),
+      routingUrl: subgraph.routing_url,
+    };
+  });
+
+  return {
+    actionType,
+    cosmoApiKey,
+    namespace,
+    featureFlags,
+    subgraphs,
+  };
+};
---
+++ src/main.ts
@@ -0,0 +1,107 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
+import { Inputs, getInputs } from './inputs';
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    const token = process.env.GITHUB_TOKEN;
+    if (!token) {
+      core.setFailed('GITHUB_TOKEN is not available.');
+      return;
+    }
+
+    const context = github.context;
+
+    const pullRequest = context.payload.pull_request;
+    if (!pullRequest) {
+      core.setFailed('This action only works with pull_requests.');
+      return;
+    }
+
+    const prNumber = pullRequest.number;
+
+    const inputs = getInputs();
+    if (!inputs) {
+      return;
+    }
+
+    setUpWgc(inputs.cosmoApiKey);
+
+    switch (inputs.actionType) {
+      case 'create':
+        await create({ inputs, prNumber });
+        break;
+      case 'update':
+        await update({ inputs, prNumber });
+        break;
+      case 'destroy':
+        await destroy({ inputs, prNumber });
+        break;
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+}
+
+/**
+ * Exports the API key as an environment variable.
+ */
+function setUpWgc(apiKey: string): void {
+  exec.exec('npm install -g wgc@latest');
+  core.exportVariable('COSMO_API_KEY', apiKey);
+}
+
+const create = async ({ inputs, prNumber }: { inputs: Inputs; prNumber: number }): Promise<void> => {
+  // Create the resources
+  let featureSubgraphsString = '';
+  for (const subgraph of inputs.subgraphs) {
+    const featureSubgraphName = `${subgraph.name}-${inputs.namespace}-${prNumber}`;
+    const command = `wgc feature-subgraph publish ${featureSubgraphName} --subgraph ${subgraph.name} --routing-url ${subgraph.routingUrl} --schema ${subgraph.schemaPath} -n ${inputs.namespace}`;
+    await exec.exec(command);
+    featureSubgraphsString += `${featureSubgraphName},`;
+    core.info(`Feature subgraph ${featureSubgraphName} using ${subgraph.name} as base subgraph is created.`);
+  }
+  for (const featureFlag of inputs.featureFlags) {
+    const command = `wgc feature-flag create ${featureFlag.name} -n ${inputs.namespace} --label ${featureFlag.labels.join(' ')} --feature-subgraphs ${featureSubgraphsString} --enabled`;
+    await exec.exec(command);
+    core.info(`Feature flag ${featureFlag.name} is created.`);
+  }
+};
+
+const update = async ({ inputs, prNumber }: { inputs: Inputs; prNumber: number }): Promise<void> => {
+  // Update the resources
+  let featureSubgraphsString = '';
+  for (const subgraph of inputs.subgraphs) {
+    const featureSubgraphName = `${subgraph.name}-${inputs.namespace}-${prNumber}`;
+    const command = `wgc feature-subgraph publish ${featureSubgraphName} --subgraph ${subgraph.name} --routing-url ${subgraph.routingUrl} --schema ${subgraph.schemaPath} -n ${inputs.namespace}`;
+    await exec.exec(command);
+    featureSubgraphsString += `${featureSubgraphName},`;
+    core.info(`Feature subgraph ${featureSubgraphName} using ${subgraph.name} as base subgraph is updated.`);
+  }
+  for (const featureFlag of inputs.featureFlags) {
+    const command = `wgc feature-flag update ${featureFlag.name} -n ${inputs.namespace} --label ${featureFlag.labels.join(' ')} --feature-subgraphs ${featureSubgraphsString}`;
+    await exec.exec(command);
+    core.info(`Feature flag ${featureFlag.name} is updated.`);
+  }
+};
+
+const destroy = async ({ inputs, prNumber }: { inputs: Inputs; prNumber: number }): Promise<void> => {
+  // Destroy the resources
+  for (const featureFlag of inputs.featureFlags) {
+    const command = `wgc feature-flag delete ${featureFlag.name} -n ${inputs.namespace} -f`;
+    await exec.exec(command);
+    core.info(`Feature flag ${featureFlag.name} is deleted.`);
+  }
+  for (const subgraph of inputs.subgraphs) {
+    const featureSubgraphName = `${subgraph.name}-${inputs.namespace}-${prNumber}`;
+    const command = `wgc subgraph delete ${featureSubgraphName} -n ${inputs.namespace} -f`;
+    await exec.exec(command);
+    core.info(`Feature subgraph ${featureSubgraphName} is deleted.`);
+  }
+};
