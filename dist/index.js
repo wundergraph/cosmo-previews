340e257dfbe95fd7f56d7295979b8079dbcca35d
JivusAyrus <suvijsurya76@gmail.com>
2024-08-16 03:20:13 +0530

    chore: update build

--- dist/index.js
+++ dist/index.js
@@ -40550,7 +40550,8 @@ const create = async ({ inputs, prNumber, changedGraphQLFiles, context, }) => {
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: prNumber,
-        body: `### Feature flags and feature subgraphs created:\n${markdownTable} \n The above feature flags are deployed, pass the feature flag name in the header(X-Feature-Flag) while making a request.  `,
+        body: `### Feature flags and feature subgraphs created:\n${markdownTable} \n \n
+    #### The above feature flags are deployed, pass the feature flag name in the header(X-Feature-Flag) while making a request.`,
     });
 };
 const update = async ({ inputs, prNumber, changedGraphQLFiles, context, }) => {
